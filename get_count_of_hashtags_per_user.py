254c586715d6b586f00fc21a9eb72fc36347943d
r00tus3r <iamakshayajayan@gmail.com>
2018-04-21 20:43:16 +0530

    get count of hashtags

---
+++ get_count_of_hashtags_per_user.py
@@ -0,0 +1,40 @@
+# twripbot - GPL - Copyright 2018 - r00tus3r
+from twython import Twython
+import utils
+
+def get_count_of_hashtags_per_user(twitter, username, hashtag):
+    max_count = 100000 # the no of tweets to be retrieved
+    earlier_tweet_id = 0
+    tweet_count = 0
+    tweet_id_list = []
+    hashtag_cnt = 0
+
+    while max_count > tweet_count:
+        if earlier_tweet_id <= 0:
+            tweets = twitter.get_user_timeline(screen_name=username, count=100, tweet_mode='extended', exclude_replies='false')
+        else:
+            tweets = twitter.get_user_timeline(screen_name=username, count=100, max_id=earlier_tweet_id-1, tweet_mode='extended', exclude_replies='false')
+
+        for tweet in tweets:
+            for i in xrange(len(tweet['entities']['hashtags'])):
+                if hashtag in tweet['entities']['hashtags'][i]['text']:
+                    hashtag_cnt += 1
+            tweet_count += 1
+            tweet_id_list.append(tweet['id'])
+
+        if(len(tweets)):
+            earlier_tweet_id = sorted(tweet_id_list)[0]
+        else:
+            break
+    print hashtag_cnt
+
+if __name__ == '__main__':
+	try:
+		twitter = utils.connect()
+	except Exception as e:
+		print e
+
+	try:
+		get_count_of_hashtags_per_user(twitter, 'aswinmguptha', 'nullcon')
+	except Exception as e:
+		print e
