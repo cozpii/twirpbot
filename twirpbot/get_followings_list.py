026b452025f692fd82cfc8de68b394661db5c2d3
r00tus3r <iamakshayajayan@gmail.com>
2018-04-23 12:06:27 +0530

    updated following list

--- twirpbot/get_followings_list.py
+++ twirpbot/get_followings_list.py
@@ -1,5 +1,4 @@
 # twripbot - GPL - Copyright 2018 - r00tus3r
-from twirpbot import connect
 
 def get_followings_list(twitter, username, count_per_req):
 	followings_list = []
@@ -7,14 +6,3 @@ def get_followings_list(twitter, username, count_per_req):
 	for i in xrange(count_per_req):
 		followings_list.append(friends['users'][i]['screen_name'])
 	return followings_list
-
-if __name__ == '__main__':
-	try:
-		twitter = connect()
-	except Exception as e:
-		print e
-
-	try:
-		print get_followings_list(twitter, 'aswinmguptha', 2)
-	except Exception as e:
-		print e
