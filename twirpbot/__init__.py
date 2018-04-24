34b30474ba0c2187c810271834915a30b14db7d0
Aswin M Guptha <aswinmguptha@gmail.com>
2018-04-24 15:24:10 +0530

    importing required functions only

--- twirpbot/__init__.py
+++ twirpbot/__init__.py
@@ -15,8 +15,8 @@ from get_tweets_count import *
 from get_user_location import *
 from get_list_count import *
 from get_user_tweet_location import *
-from get_most_common_unigrams_in_tweets import *
-from get_most_common_unigrams_in_retweets import *
+from get_most_common_unigrams_in_tweets import get_most_common_unigrams_in_tweets
+from get_most_common_unigrams_in_retweets import get_most_common_unigrams_in_retweets
 from get_verification_status import *
 from get_media_tweets_count import *
-from utils import *
+from utils import translate, connect
