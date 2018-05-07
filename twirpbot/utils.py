53997c39f60dd988de3f634b8904491a2f6ca14d
r00tus3r <iamakshayajayan@gmail.com>
2018-05-07 21:38:51 +0530

    moved translate to utils

--- twirpbot/utils.py
+++ twirpbot/utils.py
@@ -1,21 +1,28 @@
 # twirpbot - GPL - Copyright 2018 - r00tus3r
 # -*- coding: utf-8 -*-
 
-from google.cloud import translate
+import requests
+from bs4 import BeautifulSoup as bs
 from twython import Twython
 
-# To run the translate function set the path to the key file in place of the [PATH]
-# export GOOGLE_APPLICATION_CREDENTIALS=[PATH]"
-def translate(text):
-    translate_client = translate.Client()
-    target = 'en'
-    translated = translate_client.translate(text, target_language=target)
-    translatedText = translated['translatedText']
-    return translatedText
+def translate(to_translate, to_language="en", from_language="auto"):
+
+    base_link = "http://translate.google.com/m?hl=%s&sl=%s&q=%s"
+    to_translate = requests.utils.requote_uri(to_translate)
+    link = base_link % (to_language, from_language, to_translate)
+    try:
+    	result = requests.get(link)
+    except Exception as e:
+    	print("Couldn't connect to translation server\n")
+    data = result.content
+    soup = bs(data, "lxml")
+    result = soup.find_all("div", "t0")
+    result = str(result[0])[26:-6]
+    return result
 
 # Add the tokens in the tokens file
 def connect():
-    f = open('../tokens', 'r')
+    f = open('/home/r00tus3r/git/twirpbot/tokens', 'r')
     consumer_key = f.readline().strip().split('=')[1].replace(' ', '')
     consumer_key_secret = f.readline().strip().split('=')[1].replace(' ', '')
     access_token = f.readline().strip().split('=')[1].replace(' ', '')
