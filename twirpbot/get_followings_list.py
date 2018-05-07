556c53035ecf0887dbbc8e1810b29535e00ff510
r00tus3r <iamakshayajayan@gmail.com>
2018-05-07 18:12:47 +0530

    update to get all friends

--- twirpbot/get_followings_list.py
+++ twirpbot/get_followings_list.py
@@ -2,7 +2,23 @@
 
 def get_followings_list(twitter, username, count_per_req):
 	followings_list = []
-	friends = twitter.get_friends_list(screen_name=username, count=count_per_req)
-	for i in xrange(count_per_req):
-		followings_list.append(friends['users'][i]['screen_name'])
+	cur = 0
+	count = 0
+
+	while count_per_req > count:
+		if cur == 0:
+			friends = twitter.get_friends_list(screen_name=username, count=200)
+		else:
+			friends = twitter.get_friends_list(screen_name=username, count = 200, cursor = cur)
+
+		if (len(friends['users']) and friends['next_cursor'] != 0):
+			cur = friends['next_cursor']
+		else:
+			break
+
+		for i in xrange(len(friends['users'])):
+			followings_list.append(friends['users'][i]['screen_name'])
+
+		count += len(friends['users'])
+
 	return followings_list
